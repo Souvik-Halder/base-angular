08055a37a9652a18d2d0646e627fcd569f940ee9
Souvik-Halder <souvik.halder2020@gift.edu.in>
2025-04-30 02:21:06 +0530

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,22 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'base-angular';
+  name = 'Souvik Halder';
+
+  user1 = 'Souvik';
+  user2 = 'Souvik';
+
+  //The use of the interpolation is the you can use the variables of the typescripts from here to the html code
+
+  //You can use the {{}} in the html which is called the interpolation
+
+  //When we need to display the data in html file from the Ts file
+
+  // Limitation of the interpolation is
+  // - Can't declare the for loops
+  // - Can't declare the varibles in the interpolation
 }
